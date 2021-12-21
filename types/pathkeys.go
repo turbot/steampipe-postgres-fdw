67411e218c3456cc6eb6a1c7e591dcd48233ca2f
kai <kai@turbot.com>
2021-12-21 16:37:06 +0000

    Update PathKeys code to give required key columns a lower cost than than optional key columns - reimplemented with improved memory usage. Closes #116. Closes #124. Closes #117 (#125)

--- types/pathkeys.go
+++ types/pathkeys.go
@@ -1,15 +1,18 @@
 package types
 
 import (
+	"log"
 	"sort"
 	"strings"
 
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-
 	"github.com/turbot/go-kit/helpers"
-
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
 )
+
+const requiredKeyColumnBaseCost = 1
+const optionalKeyColumnBaseCost = 100
+const keyColumnOnlyCostMultiplier = 2
 
 type PathKey struct {
 	ColumnNames []string
@@ -19,60 +22,78 @@ type PathKey struct {
 func (p *PathKey) Equals(other PathKey) bool {
 	sort.Strings(p.ColumnNames)
 	sort.Strings(other.ColumnNames)
-	return strings.Join(p.ColumnNames, ",") == strings.Join(other.ColumnNames, ",")
+	return strings.Join(p.ColumnNames, ",") == strings.Join(other.ColumnNames, ",") &&
+		p.Rows == other.Rows
 }
 
-func MergePathKeys(l []PathKey, r []PathKey) []PathKey {
-	res := append([]PathKey{}, l...)
+func KeyColumnsToPathKeys(keyColumns []*proto.KeyColumn, allColumns []string) []PathKey {
+	// get the possible paths and cost for the key columns
+	columnPaths, baseCost := keyColumnsToColumnPath(keyColumns)
+	// remove key columns from allColumns
+	allColumnsExceptKeyColumns := removeKeyColumnsFromAllColumns(keyColumns, allColumns)
+	// now convert the paths to PathKeys
+	return columnPathsToPathKeys(columnPaths, allColumnsExceptKeyColumns, baseCost)
+}
 
-	for _, p := range r {
-		if !PathExistsInKeys(res, p) {
-			res = append(res, p)
+func removeKeyColumnsFromAllColumns(keyColumns []*proto.KeyColumn, allColumns []string) []string {
+	var allColumnsExceptKeyColumns = make([]string, len(allColumns)-len(keyColumns))
+	idx := 0
+	for _, c := range allColumns {
+		if !keyColumnArrayContainsColumn(keyColumns, c) {
+			allColumnsExceptKeyColumns[idx] = c
+			idx++
 		}
 	}
-	return res
+	return allColumnsExceptKeyColumns
 }
 
-func PathExistsInKeys(pathKeys []PathKey, other PathKey) bool {
-	for _, p := range pathKeys {
-		if p.Equals(other) {
+func keyColumnArrayContainsColumn(keyColumns []*proto.KeyColumn, c string) bool {
+	for _, k := range keyColumns {
+		if k.Name == c {
 			return true
 		}
 	}
 	return false
 }
 
-func KeyColumnsToPathKeys(keyColumns []*proto.KeyColumn, allColumns []string) []PathKey {
-	columnPaths := KeyColumnsToColumnPath(keyColumns)
-
-	return columnPathsToPathKeys(columnPaths, allColumns)
-}
-
-// KeyColumnsToColumnPath returns a list of all the column sets to use in path keys
-func KeyColumnsToColumnPath(keyColumns []*proto.KeyColumn) [][]string {
-	var res [][]string
+// keyColumnsToColumnPath returns a list of all the column sets to use in path keys
+func keyColumnsToColumnPath(keyColumns []*proto.KeyColumn) (columnPaths [][]string, baseCost Cost) {
 	if len(keyColumns) == 0 {
-		return res
+		return
 	}
 
-	// collect required columns - we buil da single path for all of them
-	var required []string
-	// if 'Any' key columns are specified, add them all separately
+	// collect required and optional columns - we build a single path for all of them
+	var requiredKeys, optionalKeys []string
+
 	for _, c := range keyColumns {
 		if c.Require == plugin.Required {
-			required = append(required, c.Name)
-		} else {
+			requiredKeys = append(requiredKeys, c.Name)
+		} else if c.Require == plugin.Optional {
+			optionalKeys = append(optionalKeys, c.Name)
+		} else if c.Require == plugin.AnyOf {
+			// if 'Any' key columns are specified
 			// build a path with just this column
-			res = append(res, []string{c.Name})
+			columnPaths = append(columnPaths, []string{c.Name})
 		}
 	}
-	// add required as a single path
 
-	if len(required) > 0 {
-		res = append(res, required)
+	if len(requiredKeys) > 0 {
+		// we have required keys so make the base cost CHEAP
+		baseCost = requiredKeyColumnBaseCost
+		// add required keys
+		columnPaths = append(columnPaths, requiredKeys)
+	} else {
+		baseCost = optionalKeyColumnBaseCost
+	}
+	// if there are optional keys, add a path with required keys and each optional key
+	for _, optional := range optionalKeys {
+		// NOTE: append onto optional, NOT requiredKeys - otherwise we end up reusing the underlying array
+		// and mutating values in columnPaths
+		columnPath := append([]string{optional}, requiredKeys...)
+		columnPaths = append(columnPaths, columnPath)
 	}
 
-	return res
+	return
 }
 
 func LegacyKeyColumnsToPathKeys(requiredColumns, optionalColumns *proto.KeyColumnsSet, allColumns []string) []PathKey {
@@ -85,11 +106,11 @@ func LegacyKeyColumnsToPathKeys(requiredColumns, optionalColumns *proto.KeyColum
 
 	// if there are only optional, build paths based on those
 	if len(requiredColumnSets) == 0 {
-		return columnPathsToPathKeys(optionalColumnSets, allColumns)
+		return columnPathsToPathKeys(optionalColumnSets, allColumns, 1)
 	}
 
 	// otherwise build paths based just on required columns
-	return columnPathsToPathKeys(requiredColumnSets, allColumns)
+	return columnPathsToPathKeys(requiredColumnSets, allColumns, 1)
 
 	// TODO consider whether we need to add  paths for required+optional+other columns as well??
 }
@@ -115,8 +136,8 @@ func LegacyKeyColumnsToColumnPaths(k *proto.KeyColumnsSet) [][]string {
 	}
 	return res
 }
+func columnPathsToPathKeys(columnPaths [][]string, allColumns []string, baseCost Cost) []PathKey {
 
-func columnPathsToPathKeys(columnPaths [][]string, allColumns []string) []PathKey {
 	var res []PathKey
 
 	// generate path keys each column set
@@ -125,20 +146,24 @@ func columnPathsToPathKeys(columnPaths [][]string, allColumns []string) []PathKe
 		res = append(res, PathKey{
 			ColumnNames: s,
 			// make this cheap so the planner prefers to give us the qual
-			Rows: 10,
+			Rows: baseCost * keyColumnOnlyCostMultiplier,
 		})
 		// also create paths for the columns path WITH each other column
 		for _, c := range allColumns {
 			if !helpers.StringSliceContains(s, c) {
-				columnNames := append(s, c)
+				// NOTE: create a new slice rather than appending onto s - to avoid clash between loop iterations
+				columnNames := append([]string{c}, s...)
 
 				res = append(res, PathKey{
 					ColumnNames: columnNames,
 					// make this even cheaper - prefer to include all quals which were provided
-					Rows: 1,
+					Rows: baseCost,
 				})
 			}
 		}
 	}
+
+	log.Printf("[WARN] columnPathsToPathKeys %d column paths %d all columns, %d pathkeys", len(columnPaths), len(allColumns), len(res))
+
 	return res
 }

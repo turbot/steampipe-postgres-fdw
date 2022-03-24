9b7f159257393933bae5a6c74a97b2d72b4bd076
Binaek Sarkar <binaek@turbot.com>
2022-03-24 13:41:42 +0530

    remove LDFLAGS from env

--- conversion_infos.go
+++ conversion_infos.go
@@ -1,6 +1,8 @@
 package main
 
 /*
+#cgo linux LDFLAGS: -Wl,-unresolved-symbols=ignore-all
+#cgo darwin LDFLAGS: -Wl,-undefined,dynamic_lookup
 #include "postgres.h"
 #include "common.h"
 #include "fdw_helpers.h"

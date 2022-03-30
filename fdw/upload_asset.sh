12a4aa6084513755ba043b4447f6bded0c6dea55
Puskar Basu <puskar@turbot.com>
2022-03-30 20:21:29 +0530

    update script

--- fdw/upload_asset.sh
+++ fdw/upload_asset.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-DRAFT=$(gh api -X GET /repos/{owner}/{repo}/releases -F owner=turbot -F repo=steampipe --jq '.[].draft')
+DRAFT=$(gh api -X GET /repos/{owner}/{repo}/releases -F owner=turbot -F repo=steampipe --jq 'select(.[].draft == true) | .[].draft')
 COUNT=$(echo "$DRAFT" | wc -l | tr -d ' ')
 if [[ "$COUNT" == "1" ]]; then
   TAG=$(gh api -X GET /repos/{owner}/{repo}/releases -F owner=turbot -F repo=steampipe --jq '.[].tag_name')
-  gh release upload ${TAG} ../build-Darwin/steampipe_postgres_fdw.control
+  gzip steampipe_postgres_fdw.so
+  mv steampipe_postgres_fdw.so.gz steampipe_postgres_fdw.so.darwin_arm64.gz
+  gh release upload ${TAG} steampipe_postgres_fdw.so.darwin_arm64.gz
 else
   echo "contains more than 1 draft releases"
 fi

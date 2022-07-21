a49eda01d844ce37843b150c70bb40e2ef4f1679
kai <kai@turbot.com>
2022-07-21 11:08:57 +0100

    Fix limit pushdown Update to sdk v4

--- fdw.go
+++ fdw.go
@@ -19,7 +19,7 @@ import (
 	"github.com/turbot/steampipe-postgres-fdw/version"
 
 	"github.com/hashicorp/go-hclog"
-	"github.com/turbot/steampipe-plugin-sdk/v3/logging"
+	"github.com/turbot/steampipe-plugin-sdk/v4/logging"
 	"github.com/turbot/steampipe-postgres-fdw/hub"
 	"github.com/turbot/steampipe-postgres-fdw/types"
 	"github.com/turbot/steampipe/pkg/constants"
--- types/pathkeys_test.go
+++ types/pathkeys_test.go
@@ -3,9 +3,9 @@ package types
 import (
 	"testing"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
 )
 
 type keyColumnsToPathKeysTest struct {

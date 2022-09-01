adb7243680d2479344c1f765dcf9f345561abc05
Puskar Basu <puskar@turbot.com>
2022-09-01 15:19:58 +0530

    fix CFLAGS path

--- 0_prebuild.go
+++ 0_prebuild.go
@@ -1,6 +1,7 @@
 package main
 
 /*
+#cgo darwin CFLAGS: -Ifdw -I/usr/local/include/postgresql@14/server -I/usr/local/include/postgresql@14/internal -g
 #cgo darwin CFLAGS: -Ifdw -I/usr/local/include/postgresql/server -I/usr/local/include/postgresql/internal -g
 #cgo darwin CFLAGS: -Ifdw -I/opt/homebrew/Cellar/postgresql/14.3/include/postgresql/server/ -I/opt/homebrew/Cellar/postgresql/14.3/include/postgresql/internal/ -g
 #cgo linux CFLAGS: -Ifdw -I/usr/include/postgresql/14/server -I/usr/include/postgresql/internal

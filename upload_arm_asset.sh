dac5bb57d520ebcaef47cf5fb9d8f05692ed395f
Puskar Basu <puskar@turbot.com>
2022-09-05 15:26:39 +0530

    Update build and upload scripts to accept tag

--- upload_arm_asset.sh
+++ upload_arm_asset.sh
@@ -18,16 +18,7 @@ do
   fi
 done
 
-# get the tag_names of draft releases
-TAG=$(gh api -X GET /repos/{owner}/{repo}/releases -F owner=turbot -F repo=steampipe --jq '.[] | select(.draft == true) | .tag_name')
-
-# count the number of draft releases
-COUNT=$(echo "$TAG" | wc -l | tr -d ' ')
-
-if [[ "$COUNT" == "1" ]]; then
-  gzip steampipe_postgres_fdw.so
-  mv steampipe_postgres_fdw.so.gz steampipe_postgres_fdw.so.darwin_arm64.gz
-  gh release upload ${TAG} steampipe_postgres_fdw.so.darwin_arm64.gz
-else
-  echo "contains more than 1 draft releases"
-fi
+# Zip, rename and upload the binary
+gzip steampipe_postgres_fdw.so
+mv steampipe_postgres_fdw.so.gz steampipe_postgres_fdw.so.darwin_arm64.gz
+gh release upload $1 steampipe_postgres_fdw.so.darwin_arm64.gz

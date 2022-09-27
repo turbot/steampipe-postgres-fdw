3f8f7e5f3d92002cb895461dbfe3c44e801762ca
Puskar Basu <puskar@turbot.com>
2022-09-27 18:41:05 +0530

    Update Build for Linux ARM64 workflow to error out if a command fails in ssh. Closes #248

---
+++ script_to_build.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+#function that makes the script exit, if any command fails
+exit_if_failed () {
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+}
+
+echo "Check arch and export GOROOT & GOPATH"
+uname -m
+export GOROOT=/usr/local/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo ""
+
+echo "Try an existing file"
+cat test.txt
+exit_if_failed
+echo ""
+
+echo "Check go version"
+go version
+exit_if_failed
+echo ""
+
+echo "Checkout to cloned fdw repo"
+cd steampipe-postgres-fdw
+pwd
+echo ""
+
+echo "git reset"
+git reset
+exit_if_failed
+echo ""
+
+echo "git restore all changed files(if any)"
+git restore .
+exit_if_failed
+echo ""
+
+echo "git fetch"
+git fetch
+exit_if_failed
+echo ""
+
+echo "git checkout <tag>"
+git checkout $1
+exit_if_failed
+echo ""
+
+echo "Remove existing build-Linux dir"
+rm -rf build-Linux
+exit_if_failed
+echo ""
+
+echo "Run build script"
+./build_binary.sh
+exit_if_failed
+echo ""
+
+echo "Check if binary is created"
+file build-Linux/steampipe_postgres_fdw.so
+exit_if_failed
+echo ""
+
+echo "Hallelujah!"
+exit 0

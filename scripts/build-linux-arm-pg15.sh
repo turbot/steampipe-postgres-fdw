adac2e0d3d2d0e76b13f8fe1cb8aed9a80034056
Puskar Basu <puskar@turbot.com>
2023-12-15 11:59:40 +0530

    seperate scripts for pg14 and pg15

--- scripts/build-linux-arm-pg15.sh
+++ scripts/build-linux-arm-pg15.sh
@@ -3,7 +3,7 @@
 set -e
 
 list=(
-"steampipe-plugin-github"
+"steampipe-plugin-$1"
 )
 
 if [[ ! ${MY_PATH} ]];
@@ -47,28 +47,6 @@ for item in "${list[@]}"; do
   # Step 5: Check if tar.gz file is created for v15
   if [ ! -f "steampipe_postgres_${plugin_name}.pg15.linux_arm64.tar.gz" ]; then
     echo "Error: Tar file for Postgres v15 not created."
-    exit 1
-  fi
-
-  # Step 6: Run Docker commands for Postgres FDW Builder v14
-  echo "Building Postgres FDW 14 for plugin: ${plugin_name}"
-  docker run --memory="10g" --memory-swap="16g" --rm --name sp_fdw_builder -v "$(pwd)":/tmp/ext steampipe_fdw_builder:14 make clean
-  docker run --memory="10g" --memory-swap="16g" --rm --name sp_fdw_builder -v "$(pwd)":/tmp/ext steampipe_fdw_builder:14 make standalone plugin="${plugin_name}"
-
-  # Step 7: Check if build-Linux directory is created for v14
-  if [ ! -d "build-Linux" ] || [ ! -f "build-Linux/steampipe_postgres_${plugin_name}.so" ]; then
-    echo "Error: build-Linux directory or steampipe_postgres_${plugin_name}.so not found for Postgres v14."
-    exit 1
-  fi
-
-  # Step 8: Move and tar for Postgres v14
-  echo "Move and tar for Postgres v14 for plugin: ${plugin_name}"
-  mv build-Linux steampipe_postgres_${plugin_name}.pg14.linux_arm64
-  tar -czvf steampipe_postgres_${plugin_name}.pg14.linux_arm64.tar.gz steampipe_postgres_${plugin_name}.pg14.linux_arm64
-
-  # Step 9: Check if tar.gz file is created for v14
-  if [ ! -f "steampipe_postgres_${plugin_name}.pg14.linux_arm64.tar.gz" ]; then
-    echo "Error: Tar file for Postgres v14 not created."
     exit 1
   fi
 
